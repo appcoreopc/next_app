7908b33a4ed0d0073123dbebede7d45f978132fb
jeremy woo <kepungmoin@hotmail.com>
2019-07-19 03:00:07 +1200

    routing

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,25 @@
 import Header from '../components/header';
 import Layout from '../components/MyLayout';
 
-const Index = () => (
-  <Layout>  
-    <p>Hello - Next.js</p>
-  </Layout>
+import Link from 'next/link';
+
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 );
 
-export default Index;
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  );
+}
